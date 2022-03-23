60174e7f23fb3aff5b419d8eab3290da36a1a39b
Ali Awan <231522965@formanite.fccollege.edu.pk>
2022-03-24 03:32:07 +0500

    added Week_1

---
+++ Week_1/reviewQuiz.py
@@ -0,0 +1,16 @@
+# Write a short Python function,
+# isMultiple(n,m), which takes two integer values 
+# and returns True if n is a multiple of m, 
+# so n = m*i for some integer i and 
+# False otherwise. 
+
+def isMultiple(n,m):
+    if n % m == 0:
+        return print(True)
+    else: 
+        return print(False)
+
+
+isMultiple(8,2) 
+
+
