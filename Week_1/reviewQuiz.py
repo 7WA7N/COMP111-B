169170e0b2abb3c80bc081fb57b9d5c9d59c77c9
Ali Awan <231522965@formanite.fccollege.edu.pk>
2022-03-26 16:56:44 +0500

    more homework

--- Week_1/reviewQuiz.py
+++ Week_1/reviewQuiz.py
@@ -1,3 +1,5 @@
+
+#! Question 1 
 # Write a short Python function,
 # isMultiple(n,m), which takes two integer values 
 # and returns True if n is a multiple of m, 
@@ -13,3 +15,13 @@ def isMultiple(n,m):
 
 
 isMultiple(8,2) 
+
+#! Question 2 
+# The sum of all even numbers 
+# between 2 and 100 (inclusive).
+
+def sumEven():
+    print(sum(range(2,102,2)))
+
+sumEven()
+
