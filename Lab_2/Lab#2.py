ee9f2b430fd1f7026e2976295e15b529972af15e
Ali Awan <231522965@formanite.fccollege.edu.pk>
2022-03-27 17:43:59 +0500

    completed Lab2, 4/4

--- Lab_2/Lab#2.py
+++ Lab_2/Lab#2.py
@@ -18,6 +18,35 @@ def wc(filename):
 wc('sample.txt')
 
     
+#! Question 2 
+
+def square1(x):
+    return x*x
+
+def square2(x):
+    print(x*x)
+
+a = square1(4)
+b = square2(4)
+
+
+#! Question 3 
+
+def summation(n): 
+    total, k = 0, 1 
+    while k <= n: 
+        total, k = total + k*k*k, k + 1 
+    return total 
+result = summation (3)
+
+#! Question 4
+
+x = 5
+def square(x):
+ return x*x
+def double(x):
+ return square(x+1) - square(x) - 1
+double(x)
 
 
     
