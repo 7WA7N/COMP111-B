afb4d04e1c90b75fc0977ab51fafff68a336b4bb
Ali Awan <231522965@formanite.fccollege.edu.pk>
2022-04-04 22:13:45 +0500

    Finished Week 1

--- Week_1/reviewQuiz.py
+++ Week_1/reviewQuiz.py
@@ -1,3 +1,7 @@
+# 231522965
+# Muhammad Ali Awan
+# COMP-111-B
+# Lab 1 
 
 #! Question 1 
 # Write a short Python function,
@@ -12,16 +16,43 @@ def isMultiple(n,m):
         return print(True)
     else: 
         return print(False)
-
-
 isMultiple(8,2) 
 
 #! Question 2 
 # The sum of all even numbers 
 # between 2 and 100 (inclusive).
 
-def sumEven():
-    print(sum(range(2,102,2)))
+def sumAllEven():
+    sum = 0
+    for x in range(2,102,2):
+        sum += x 
+    return sum
+sumAllEven()
 
-sumEven()
+#! Question 3 
+# Write down the sum of all squares between
+# 1 and 100, inclusive. Using a function. 
+
+def sumOfsquares():
+    total = 0 
+    for x in range(1,101):
+        square = x*x
+        total = total + square
+    return print(total)
+sumOfsquares()
+
+#! Question 4 
+# The sum of all Odd Numbers, between
+# a and b, inclusive. 
+
+def sumOfOdds(a,b):
+    total = 0 
+    for x in range(a,b+1):
+        if x % 2 != 0:
+            total += x
+    return total
+
+sumOfOdds(1,100)
+
+
 
