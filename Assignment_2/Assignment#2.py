5e876bb1c26b5f5b660585a7aa3fb2abd6df37e4
Ali Awan <79001319+7WA7N@users.noreply.github.com>
2022-05-28 17:24:24 +0500

    Finished Task 2

--- Assignment_2/Assignment#2.py
+++ Assignment_2/Assignment#2.py
@@ -70,32 +70,61 @@ class Player():
 
 class Batsman(Player):
     def __init__(self,Name,Matches):
-        super().__init__(Name,Matches)
-        for i in Matches:
-            self.PerMatchScore = []
-            self.PerMatchScore = int(input(f"Enter the score of the match {i}:"))
-            self.TotalScore += self.PerMatchScore
-            
-        self.Average = self.Average
+      super().__init__(Name,Matches)
+      self.PerMatchScore = []
+      i = 0
+      while i < Matches:
+        score = int(input(f"Enter the score of match {i+1} for {self.Name} :"))
+        self.PerMatchScore.append(score)
+        i += 1
+          
+      self.TotalScore = sum(self.PerMatchScore)    
 
     def CalculateAverage(self):
-        self.Average = self.TotalScore/len(self.PerMatchScore)
-        return self.Average
+      self.Average = self.TotalScore/len(self.PerMatchScore)
+      return print(f'{self.Name}\'s average score is {self.Average}')
+    
+    def Display(self):
+      print('Name:', self.Name)
+      print('Matches:', self.Matches)
+      print('Per Match Score:', self.PerMatchScore)
+      print('Total Score:', self.TotalScore)
+    
+    def PlayerType(self):
+      self.PlayerType = 'Batsman'
+      return print(self.PlayerType)
 
 class Bowler(Player):
-    
     def __init__(self,Name,Matches):
         super().__init__(Name,Matches)
-        for i in Matches:
-            self.PerMatchWickets = []
-            self.PerMatchWickets = int(input(f"Enter the wickets taken in match {i}:"))
-            self.NoOfWickets += self.NoOfWickets
+        self.PerMatchWickets = []
+        i = 0
+        while i < Matches:
             
-        self.Average = self.Average
-    
+            wickets = int(input(f"Enter the wickets taken in match {i+1} for {self.Name} :"))
+            self.PerMatchWickets.append(wickets)
+            i += 1
+          
+        self.NoOfWickets = sum(self.PerMatchWickets)
+
     def CalculateAverage(self):
-        self.Average = self.TotalScore/len(self.PerMatchScore)
-        return self.Average
+        self.Average = self.NoOfWickets/len(self.PerMatchWickets)
+        return print(f'{self.Name}\'s average wickets taken is {self.Average}')
+
+    def Display(self):
+      print('Name:', self.Name)
+      print('Matches:', self.Matches)
+      print('Per Match Wickets:', self.PerMatchWickets)
+      print('No Of Wickets:', self.NoOfWickets)
+
+    def PlayerType(self):
+      self.PlayerType = 'Bowler'
+      return print(self.PlayerType)
+
+      
+      
+
+      
 
     
  
@@ -124,35 +153,18 @@ class Bowler(Player):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Main Function 
 def main():
     
-    print("Task 1")
+    print("## Task 1 ##")
     truck1 = Truck("Ford",4,"Ali Awan",5000,2000,25,150,'Sargodah','Khushab',2300)
-    truck2 = Truck("BMW",4,"Ali Awan",2400,2000,360,2400,'Hafeezabad','Karachi',4200)
-    bus1 = Bus("Mercedes",4,"Ali Awan",100,25,25,150,'Lari Adda','Multan Chungi',500)
-    bus2 = Bus("Suzuki Wagon",4,"Ali Awan",100,100,21,150,'Hafeez Center','University of Toronto',6500)
+    truck2 = Truck("BMW",4,"Ahmed Awan",2400,2000,360,2400,'Hafeezabad','Karachi',4200)
+    bus1 = Bus("Mercedes",4,"Fazal Awan",100,25,25,150,'Lari Adda','Multan Chungi',500)
+    bus2 = Bus("Suzuki Wagon",4,"Kashif Awan",100,100,21,150,'Hafeez Center','University of Toronto',6500)
 
     data = [truck1,truck2,bus1,bus2]
     for objects in data:
+        print('\n')
         print('---------------------------------------------------')
         print('Manufacturer:',objects.Manufacturer)
         print('Cylinders:',objects.Cylinders)
@@ -162,6 +174,20 @@ def main():
         print('---------------------------------------------------')
         print('\n')
 
-    # Task 2
+    print('## Task 2 ##')
+    print('\n')
+  
+    batsman1 = Batsman('Shahid Afridi',3)
+    bowler1 = Bowler('Gul Khan',3)
+
+    for objects in [batsman1,bowler1]:
+      print('\n')
+      objects.Display()
+      objects.CalculateAverage()
+      objects.PlayerType()
+  
+  
+  
+      
 
 main()
